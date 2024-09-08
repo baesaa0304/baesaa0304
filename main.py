8bea4922d3e8573315361d09bb7996975f84f0e6
Baesunyoung <baesaa0304@naver.com>
2024-09-08 22:03:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import feedparser, time
-
-URL="https://baesaa0304.tistory.com/rss"
+ 
+URL = "https://baesaa0304.tistory.com/rss"
 RSS_FEED = feedparser.parse(URL)
-MAX_POST=7
-
+MAX_POST = 5
+ 
 markdown_text = """
-## The latest blog
-""" # list of blog posts will be appended here
-
-
+## ✅ Latest Blog Post
+ 
+"""  # list of blog posts will be appended here
+ 
 for idx, feed in enumerate(RSS_FEED['entries']):
     if idx > MAX_POST:
         break
     else:
         feed_date = feed['published_parsed']
         markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
-
-f = open("README.md",mode="w", encoding="utf-8")
+        
+f = open("README.md", mode="w", encoding="utf-8")
 f.write(markdown_text)
 f.close()
