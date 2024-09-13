d20b9a349ccc5f7f3f7a15632a306d974f6f6027
baesaa0304 <baesaa0304@naver.com>
2024-09-13 09:37:12 +0900

    리드미 오류 수정

--- readmeUpdate.js
+++ readmeUpdate.js
@@ -18,7 +18,7 @@ const parser = new Parser({
   const feed = await parser.parseURL("https://baesaa0304.tistory.com/rss"); // 수정
  
   // 최신 5개의 글의 제목과 링크를 추가할 텍스트 생성
-  let latestPosts = "###📝Latest Blog Posts\n\n";
+  let latestPosts = "### 📝Latest Blog Posts\n\n";
   for (let i = 0; i < 5 && i < feed.items.length; i++) {
     let { title, link } = feed.items[i];
     link = link.startsWith('http://') ? 'https://' + link.slice(7) : link; // 추가
@@ -26,9 +26,9 @@ const parser = new Parser({
   }
  
   // 기존 README.md에 최신 블로그 포스트 추가
-  const newReadmeContent = readmeContent.includes("###📝Latest Blog Posts")
+  const newReadmeContent = readmeContent.includes("### 📝Latest Blog Posts")
     ? readmeContent.replace(
-        /###📝Latest Blog Posts[\s\S]*?(?=\n\n## |\n$)/,
+        /### 📝Latest Blog Posts[\s\S]*?(?=\n\n## |\n$)/,
         latestPosts
       )
     : readmeContent + latestPosts;
